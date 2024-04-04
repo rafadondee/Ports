e913ac462c0936bf988a7af7a10fc11aab0c19a9
RAFAEL CARDOSO DONDE <rafael_donde@estudante.sesisenai.org.br>
2024-04-04 12:20:11 -0300

    Atualização: Add de coisas que faltavam

--- src/pages/matematica.jsx
+++ src/pages/matematica.jsx
@@ -18,19 +18,18 @@ function Matematica() {
           <button className="botao-materias" onClick={() => handleClick('/materias')}>Matérias</button>
           <button className="botao-ppe" onClick={() => handleClick('/ppe')}>PPE</button>
           <button className="botao-arcosEangulos" onClick={() => window.open('/Av1 matemática - história .pdf', '_blank')}>AV 1</button>
-
         </div>
-        <a href="/segundo.pdf" target="_blank" rel="noopener noreferrer">AV 1</a>
       
       </header>
       <a className="titulo-header" href='/'>Rafa</a>
-     <h1>Clique aqui para acessar a <br /> primeira avaliação de Matemática!</h1> 
-     <div class="loader">
-  <div class="box1"></div>
-  <div class="box2"></div>
-  <div class="box3"></div>
-</div>  
-<p className='paragrafo-av1Mtm'>Essa atividade foi feita com o objetivo<br />  de compreender melhor o uso de ângulos<br />  e arcos em situações cotidianas.<br /> Envolveu as habilidades H5 e H6.</p>
+      <h1>Clique aqui para acessar a <br /> primeira avaliação de Matemática!</h1>
+      <div className="loader">
+        <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+      </div>
+      <p className='paragrafo-av1Mtm'>Essa atividade foi feita com o objetivo<br />  de compreender melhor o uso de ângulos<br />  e arcos em situações cotidianas.<br /> Envolveu as habilidades H5 e H6.</p>
+      <iframe className= 'video-gabigol' src="https://drive.google.com/file/d/18o8Xyt2uA48UvwHs0zfAMbo0qxMnBroD/preview" allow="autoplay"></iframe>
     </div>
   );
 }
